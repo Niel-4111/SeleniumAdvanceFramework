cb2481fac70aef0d417804d4696ab3f4c94a48a3
niles <nilesh1998sonawane@gmail.com>
2025-02-02 01:02:56 +0530

    Project smoke

---
+++ src/test/java/com/apiautomation/tests/sample.java
@@ -0,0 +1,13 @@
+package com.apiautomation.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class sample {
+
+    @Test
+    public void test_smoke(){
+        Assert.assertTrue(true);
+    }
+
+}
